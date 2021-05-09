9c6143bec3da7df58eb9ffa1460a6d59965f5f82
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-09 18:19:17 +0700

    basic design for header, footer and route to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Angular components
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+
+// Nebular
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbActionsModule,
+} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+// Angular components
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -23,7 +32,9 @@ import { FooterComponent } from './footer/footer.component';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbActionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
