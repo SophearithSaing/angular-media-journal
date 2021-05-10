80544e13bfe36df9b6f2b0dc32eb0b8c588f307a
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-11 02:16:08 +0700

    add models

---
+++ src/app/models/network.model.ts
@@ -0,0 +1,8 @@
+export interface Network {
+  headquarters: string;
+  homepage: string;
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
--- src/app/models/savedMovie.model.ts
+++ src/app/models/savedMovie.model.ts
@@ -1,3 +1,9 @@
-export interface SavedMovie {
+import { Movie } from './movie.model';
 
+export interface SavedMovie {
+  movie: Movie;
+  startDate: Date;
+  endDate: Date;
+  network: string;
+  rating: number;
 }
