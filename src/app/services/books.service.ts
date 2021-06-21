f4d2a8faf69f2bf44882ed4b6b4e053736fdc5a7
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-06-21 23:43:21 +0700

    get books

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Book } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  apiUrl = environment.bookApiUrl;
+  apiKey = environment.bookApiKey;
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(query: string) {
+    const url = `${this.apiUrl}?q=${query}&key=${this.apiKey}`;
+    return this.http.get<{ totalItems: number, items: Array<Book> }>(url);
+  }
+
+  getBook(id: string) {
+    const url = `${this.apiUrl}/q=${id}&key=${this.apiKey}`;
+    return this.http.get<Book>(url);
+  }
 }
