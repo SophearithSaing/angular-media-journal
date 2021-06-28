5e3fbd8d3ce777b008f050759273dfa64bd35063
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-06-28 22:50:41 +0700

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
 // Angular components
 import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '**', component: HomeComponent }
+  { path: '**', component: HomeComponent },
+  { path: '/search', component: SearchComponent},
+  { path: '/profile', component: ProfileComponent },
+  { path: '/movies', component: MyMoviesComponent},
+  { path: '/music', component: MusicComponent },
+  { path: '/book', component: BooksComponent },
+
 ];
 
 @NgModule({
