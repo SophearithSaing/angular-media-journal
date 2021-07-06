c7500a45fe998c19430ef554f1dc9ed8559ffb35
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-07-06 23:55:23 +0700

    update music model

--- src/app/models/music.model.ts
+++ src/app/models/music.model.ts
@@ -1,3 +1,72 @@
 export interface Music {
+  id: string;
+  readable: boolean;
+  title: string;
+  title_short: string;
+  title_version: string;
+  isrc: string;
+  link: string;
+  share: string;
+  duration: string;
+  track_position: number;
+  disk_number: number;
+  rank: string;
+  release_date: string;
+  explicit_lyrics: boolean;
+  explicit_content_lyrics: number;
+  explicit_content_cover: number;
+  preview: string;
+  bpm: number;
+  gain: number;
+  available_countries: string[];
+  contributors: Contributor[];
+  md5_image: string;
+  artist: Artist;
+  album: Album;
+  type: string;
+}
 
+interface Contributor {
+  id: number;
+  name: string;
+  link: string;
+  share: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl: string;
+  radio: boolean;
+  tracklist: string;
+  type: string;
+  role: string;
+}
+
+interface Artist {
+  id: string;
+  name: string;
+  link: string;
+  share: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl: string;
+  radio: boolean;
+  tracklist: string;
+  type: string;
+}
+interface Album {
+  id: string;
+  title: string;
+  link: string;
+  cover: string;
+  cover_small: string;
+  cover_medium: string;
+  cover_big: string;
+  cover_xl: string;
+  md5_image: string;
+  release_date: string;
+  tracklist: string;
+  type: string;
 }
