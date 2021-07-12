67fdc496fddc83c7e5ce4e414c85da25a443ffbb
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-07-12 23:37:37 +0700

    Import env variable to music services

--- src/app/services/musics.service.ts
+++ src/app/services/musics.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Music } from '../models/music.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicsService {
 
-  constructor() { }
+  apiUrl = environment.musicApiUrl;
+  apiHost = environment.musicApiHost;
+  apiKey = environment.musicApiKey;
+  db = environment.firebaseDB;
+
+  constructor(private http: HttpClient, private firestore: AngularFirestore) { }
 }
